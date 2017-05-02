309f992e356d770422acaa9296fbbe2c25bc130e
hogelog <konbu.komuro@gmail.com>
2017-05-03 01:11:57 +0900

    Use babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
           use: ["css-loader", "sass-loader"],
         })
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
   },
   plugins: [
