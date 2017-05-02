c614a5caeba66af75c6c4cebd12656feeb0c2625
hogelog <konbu.komuro@gmail.com>
2017-05-03 00:55:53 +0900

    Use webpack as external pipeline

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,1 @@
-// This is where it all goes :)
+require('../stylesheets/site.scss');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    site: "./source/javascripts/site.js",
+  },
+  output: {
+    filename: "javascripts/[name].bundle.js",
+    path: path.resolve(__dirname, ".tmp/dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          use: ["css-loader", "sass-loader"],
+        })
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin("stylesheets/[name].bundle.css"),
+  ]
+};
